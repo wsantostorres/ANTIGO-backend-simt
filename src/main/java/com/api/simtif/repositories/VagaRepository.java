98005743e35da29eb8e65435f05f8038c4a26f5c
Wanderson Escolar <santos.torres@escolar.ifrn.edu.br>
2023-06-07 13:20:44 -0300

    Configurando CORS

--- src/main/java/com/api/simtif/repositories/VagaRepository.java
+++ src/main/java/com/api/simtif/repositories/VagaRepository.java
@@ -2,7 +2,9 @@ package com.api.simtif.repositories;
 
 import com.api.simtif.models.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
 
 }
