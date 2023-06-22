39866f6ca16bc7ff3d482c8c090719bcd4f66d70
Wanderson Escolar <santos.torres@escolar.ifrn.edu.br>
2023-06-22 11:29:50 -0300

    Relacionando Curso pelo controller de vagas

--- src/main/java/com/api/simtif/controllers/VagaController.java
+++ src/main/java/com/api/simtif/controllers/VagaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,19 +34,21 @@ public class VagaController {
 
     @PostMapping("/vagas/")
     public ResponseEntity<Vaga> saveVaga(@RequestBody Vaga vaga) {
-        List<Curso> cursos = vaga.getCursos();
-        for (Curso curso : cursos) {
+        List<Curso> cursos = new ArrayList<>();
+        for (Curso curso : vaga.getCursos()) {
             Optional<Curso> cursoOptional = cursoRepository.findById(curso.getId());
             if (cursoOptional.isPresent()) {
                 Curso existingCurso = cursoOptional.get();
                 existingCurso.getVagas().add(vaga);
-                cursoRepository.save(existingCurso);
+                cursos.add(existingCurso);
             }
         }
+        vaga.setCursos(cursos);
 
         Vaga savedVaga = vagaRepository.save(vaga);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedVaga);
     }
+
 
     @PutMapping("/vagas/{id}/")
     public Vaga updateVaga(@PathVariable long id, @RequestBody Vaga vaga){
