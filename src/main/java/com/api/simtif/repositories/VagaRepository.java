908d50c81f202f6103343ea60735d0ad2d807acc
David Cauã <davidback.abc1@gmail.com>
2023-06-24 15:52:52 -0300

    busca por titulo e filtragem pelo status

--- src/main/java/com/api/simtif/repositories/VagaRepository.java
+++ src/main/java/com/api/simtif/repositories/VagaRepository.java
@@ -1,10 +1,20 @@
 package com.api.simtif.repositories;
 
 import com.api.simtif.models.Vaga;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*")
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
+
+    @Query(value = "select v from Vaga v where upper(trim(v.titulo)) like %?1%") // %?1% referece ao parametro titulo
+    List<Vaga> buscarTitulo(String titulo);
+
+    @Query(value = "select v from Vaga v where v.status = ?1")
+    List<Vaga> buscarStatus(int status);
 
 }
